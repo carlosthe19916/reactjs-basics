8588dc86672cd436438d8089cbc511cf2924bfed
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-05-10 17:57:53 +0200

    add components

---
+++ src/MyFunctionComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyFunctionComponent = () => {
+  return (
+    <div>
+      <h1>I am a function component</h1>
+    </div>
+  );
+}
+
+export default MyFunctionComponent;
